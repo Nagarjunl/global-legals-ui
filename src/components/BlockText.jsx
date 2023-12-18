c7e267ebb05f8c2b52de4d6e4b2f60665994e44e
gopalakrishnan <semsampathkumar@gmail.com>
2023-12-18 16:59:35 +0530

    fixed home screen component alignment

--- src/components/BlockText.jsx
+++ src/components/BlockText.jsx
@@ -1,13 +1,14 @@
-import React from "react";
 import { FaRegCheckCircle } from "react-icons/fa";
 const BlockText = ({ heading, content }) => {
   return (
     <>
-      <div className="flex gap-3 text-start">
-        <FaRegCheckCircle className="h-[28px] w-[28px] text-gray-600" />
-        <p className="text-black text-xl font-bold self-stretch grow shrink basis-auto max-md:max-w-full">
-          {heading}
-        </p>
+      <div className="flex flex-col  gap-2 text-start">
+        <div className="flex gap-2">
+          <FaRegCheckCircle className="h-[28px] w-[28px] text-gray-600" />
+          <p className="text-black text-xl font-bold self-stretch grow shrink basis-auto max-md:max-w-full">
+            {heading}
+          </p>
+        </div>
 
         <p className="text-trueGray-600 mt-2 font-semibold font-circular-std text-base  font-hairline leading-normal">
           {content}
