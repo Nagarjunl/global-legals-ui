c542e3c3ddfea0cbfa9d910df41ac6e58bd28864
Prabakaran.M <karanpk0108@gmail.com>
2023-12-20 00:01:29 +0530

    Added left side bar

--- src/pages/CreatePassword.jsx
+++ src/pages/CreatePassword.jsx
@@ -1,56 +1,21 @@
 import logo from "../assets/logo.png";
 import Paperplane from "../assets/paper-plane.png";
-import avatar1 from "../assets/Rectangle3.png";
-import avatar2 from "../assets/Rectangle2.png";
-import avatar3 from "../assets/Rectangle4.png";
 import "../App.css";
 import PrimaryButton from "../components/PrimaryButton";
 import Input from "../components/Input";
+import LeftsideBar from "../components/Leftside-Bar";
 
 const CreatePassword = () => {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 min-h-screen">
-      <div className="hidden lg:block bg-blue-600 py-3 mb-0">
-        <div className="container pl-14 pt-10">
-          <div className="mb-10 block">
-            <h2 className="text-white pb-2 font-semibold text-3xl">
-              Welcome to Globallegals
-            </h2>
-            <p className="text-white">
-              Connecting you with the professionals you need, right in
-              <br />
-              <span> your neighborhood!</span>
-            </p>
-          </div>
-
-          <div className="background_image fixed w-[420px] h-[400px]">
-            <img
-              className="inline-block h-16 w-16 rounded-full absolute right-[50%] top-16"
-              src={avatar1}
-              alt=""
-            />
-            <img
-              className="inline-block h-12 w-12 rounded-full absolute bottom-[8rem] left-6"
-              src={avatar2}
-              alt=""
-            />
-            <img
-              className="inline-block h-14 w-14 rounded-full absolute bottom-16 right-[10%]"
-              src={avatar3}
-              alt=""
-            />
-          </div>
-        </div>
+      <div className=" max-lg:hidden lg:flex bg-blue-600 xl:flex items-center justify-center ">
+        <LeftsideBar />
       </div>
 
       <div className="flex flex-col justify-center px-4 py-12 gap-10 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
         <div className="mx-auto w-full max-w-sm lg:w-96">
           <div className="flex items-center">
             <img className="h-10 w-auto" src={logo} alt="Your Company" />
-            <h2 className="ml-2 text-2xl font-bold leading-9 tracking-tight text-blue-600">
-              Global <br />
-              Legals
-            </h2>
           </div>
 
           <div>
