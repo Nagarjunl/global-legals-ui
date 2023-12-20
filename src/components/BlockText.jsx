5ab5d8d84e0d5e777ca91227522e2b61ac67c6ff
barathan <barathanit7@gmail.com>
2023-12-20 22:26:36 +0530

    home page rework complete

--- src/components/BlockText.jsx
+++ src/components/BlockText.jsx
@@ -2,14 +2,14 @@ import { FaRegCheckCircle } from "react-icons/fa";
 const BlockText = ({ heading, content }) => {
   return (
     <>
-      <div className="mt-12 grid items-end ">
+      <div className="grid items-end mt-10">
         <div className="flex gap-2 max-md:text-start max-md:pl-2 ">
           <FaRegCheckCircle className="h-[28px] w-[28px] text-gray-600 max-md:hidden" />
           <p className="  text-xl font-bold  grow shrink basis-auto max-md:max-w-full">
             {heading}
           </p>
         </div>
-        <div className="pr-36 mt-2 max-md:pr-2 pl-2">
+        <div className="pr-36 mt-2 max-md:pr-2">
           <p className="text-trueGray-600  font-circular-std text-base font-hairline ">
             {content}
           </p>
--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -11,8 +11,8 @@ const ServiceCard = ({ image, content, contentBold }) => {
               src={image}
               alt=" image"
             />
-            <div>
-              <figcaption className="absolute left-5 bottom-7 p-7 max-lg:p-5   text-center  bg-white rounded-full py-2  shadow-lg shadow-black/5 saturate-200 max-sm:p-3  ">
+            <div className="flex justify-center">
+              <figcaption className="absolute  bottom-7 p-7 max-lg:p-5   text-center  bg-white rounded-full py-2  shadow-lg shadow-black/5 saturate-200 max-sm:p-3  ">
                 <p className="font-sans text-base antialiased font-normal  text-gray-700">
                   {content}
                 </p>
