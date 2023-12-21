dfba898d0b167db04da2e8a7e3f47eb75f8ef75d
gopalakrishnan <semsampathkumar@gmail.com>
2023-12-21 16:01:43 +0530

    fixed stepper design and recaptcha button issue

--- src/pages/SignUpForms/BondBailsman.jsx
+++ src/pages/SignUpForms/BondBailsman.jsx
@@ -1,12 +1,13 @@
-import BusinessList from "../../components/bussinesslist";
-import LawField from "../../components/lawField";
 import GoogleImage from "../../assets/Google-image.png";
-import Experence from "../../components/Experence";
+import ReCAPTCHA from "react-google-recaptcha";
 
-function BusinessInformation() {
+function BondBailsman() {
+  const handleChange = () => {
+    console.log("ReCaptcha");
+  };
   return (
     <>
-      <div className="mx-auto  px-4 max-w-7xl sm:px-6 lg:px-8">
+      <div className="mx-auto px-4 max-w-7xl sm:px-6 lg:px-12">
         <div>
           <h3 className=" font-semibold leading-6 text-2xl mt-11 text-gray-900">
             Select your profession
@@ -18,22 +19,116 @@ function BusinessInformation() {
               to your specific needs. Let&apos;s ensure we <br /> provide you
               with the best experience on Globallegals
             </p>
-            <BusinessList />
+            <div>
+              <select
+                id="location"
+                name="location"
+                className="block w-full mt-4 rounded-md border-0 py-3 pl-4 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                defaultValue=""
+              >
+                <option value="" disabled hidden>
+                  Select from dropdown
+                </option>
+
+                <option>United States</option>
+                <option>Canada</option>
+                <option>Mexico</option>
+              </select>
+            </div>{" "}
           </div>
         </div>
         <div>
-          <Experence />
-        </div>
-        <div>
-          <LawField />
-        </div>
-        <div>
-          <div className=" grid grid-cols-1 gap-x-6  sm:grid-cols-6">
+          <div className="mt-3 grid grid-cols-1 gap-x-6  sm:grid-cols-6">
             <h3 className=" font-semibold leading-6 text-2xl mt-12 text-gray-900 sm:col-span-6">
-              Insurance Information
+              Business Information
             </h3>
             <div className="sm:col-span-3">
-              <h5 className="mt-2">Insurance Policy Number</h5>
+              <h5 className="mt-2">Full Legal Business Name</h5>
+              <div className="mt-2">
+                <input
+                  type="text"
+                  name="first-name"
+                  id="first-name"
+                  autoComplete="given-name"
+                  placeholder="Enter Name"
+                  className="block px-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div className="sm:col-span-3">
+              <h5 className="mt-2">Business Address</h5>
+              <div className="mt-2">
+                <input
+                  type="text"
+                  name="first-name"
+                  id="first-name"
+                  autoComplete="given-name"
+                  placeholder="Business Address"
+                  className="block px-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div className="sm:col-span-3">
+              <h5 className="mt-2">Business Email Address</h5>
+              <div className="mt-2">
+                <input
+                  type="text"
+                  name="first-name"
+                  id="first-name"
+                  autoComplete="given-name"
+                  placeholder="Enter Email"
+                  className="block px-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div className="sm:col-span-3">
+              <h5 className="mt-2">Business Phone number</h5>
+              <div className="mt-2">
+                <input
+                  type="text"
+                  name="first-name"
+                  id="first-name"
+                  autoComplete="given-name"
+                  placeholder="Enter number"
+                  className="block w-full px-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div className="sm:col-span-3">
+              <h5 className="mt-2">Company website</h5>
+              <div className="mt-2">
+                <input
+                  type="text"
+                  name="first-name"
+                  id="first-name"
+                  autoComplete="given-name"
+                  placeholder="company website"
+                  className="block px-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div className="sm:col-span-3">
+              <h5 className="mt-2">Owner Agent Information</h5>
+              <div className="mt-2">
+                <input
+                  type="text"
+                  name="first-name"
+                  id="first-name"
+                  autoComplete="given-name"
+                  placeholder="Agent Information"
+                  className="block w-full px-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+          </div>{" "}
+        </div>
+        <div>
+          <div className="grid grid-cols-1 gap-x-6  sm:grid-cols-6">
+            <h3 className=" font-semibold leading-6 text-2xl mt-12 text-gray-900 sm:col-span-6">
+              Licensing Credentials
+            </h3>
+            <div className="sm:col-span-3">
+              <h5 className="mt-2">License Number</h5>
               <div className="mt-2">
                 <input
                   type="text"
@@ -41,44 +136,38 @@ function BusinessInformation() {
                   id="first-name"
                   placeholder="Enter number"
                   autoComplete="given-name"
-                  className="block px-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="block w-full px-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
             <div className="sm:col-span-3">
-              <h5 className="mt-2">Insurance Provider</h5>
+              <h5 className="mt-2">Date of Licensing</h5>
               <div className="mt-2">
                 <input
                   type="text"
                   name="first-name"
                   id="first-name"
-                  placeholder="Enter date"
                   autoComplete="given-name"
+                  placeholder="Enter date"
+                  className="block w-full px-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div className="sm:col-span-3">
+              <h5 className="mt-2">Licensing Authority</h5>
+              <div className="mt-2">
+                <input
+                  type="text"
+                  name="first-name"
+                  id="first-name"
+                  autoComplete="given-name"
+                  placeholder="Licensing Authority"
                   className="block px-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
-            <div className="sm:col-span-3 ">
-              <h5 className="mt-2">Coverage Amount</h5>
-
-              <div className="mt-2">
-                <select
-                  id="country"
-                  name="country"
-                  autoComplete="country-name"
-                  className="block w-full rounded-md border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600  sm:text-sm sm:leading-6"
-                >
-                  <option value="" disabled selected hidden>
-                    Select from dropdown
-                  </option>
-                  <option>United States</option>
-                  <option>Canada</option>
-                  <option>Mexico</option>
-                </select>
-              </div>
-            </div>
             <div className="sm:col-span-3">
-              <h5 className="mt-2">Experience Date of Insurance</h5>
+              <h5 className="mt-2">Bonding Capacity</h5>
               <div className="mt-2">
                 <input
                   type="text"
@@ -86,9 +175,72 @@ function BusinessInformation() {
                   id="first-name"
                   placeholder="Bonding capacity"
                   autoComplete="given-name"
-                  className="block w-full px-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="block px-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
+            </div>
+          </div>
+        </div>{" "}
+        <div className="grid grid-cols-1 gap-x-6  sm:grid-cols-6">
+          <h3 className=" font-semibold leading-6 text-2xl mt-12 text-gray-900 sm:col-span-6">
+            Insurance Information
+          </h3>
+          <div className="sm:col-span-3">
+            <h5 className="mt-2">Insurance Policy Number</h5>
+            <div className="mt-2">
+              <input
+                type="text"
+                name="first-name"
+                id="first-name"
+                placeholder="Enter number"
+                autoComplete="given-name"
+                className="block px-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div className="sm:col-span-3">
+            <h5 className="mt-2">Insurance Provider</h5>
+            <div className="mt-2">
+              <input
+                type="text"
+                name="first-name"
+                id="first-name"
+                placeholder="Enter date"
+                autoComplete="given-name"
+                className="block px-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div className="sm:col-span-3 ">
+            <h5 className="mt-2">Coverage Amount</h5>
+
+            <div className="mt-2">
+              <select
+                id="country"
+                name="country"
+                autoComplete="country-name"
+                className="block w-full rounded-md border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600  sm:text-sm sm:leading-6"
+              >
+                <option value="" disabled selected hidden>
+                  Select from dropdown
+                </option>
+                <option>United States</option>
+                <option>Canada</option>
+                <option>Mexico</option>
+              </select>
+            </div>
+          </div>
+          <div className="sm:col-span-3">
+            <h5 className="mt-2">Experience Date of Insurance</h5>
+            <div className="mt-2">
+              <input
+                type="text"
+                name="first-name"
+                id="first-name"
+                placeholder="Bonding capacity"
+                autoComplete="given-name"
+                className="block w-full px-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
             </div>
           </div>
         </div>
@@ -163,14 +315,13 @@ function BusinessInformation() {
               showcase your expertise. Click below to
               <br /> elevate your professional reputation on Global legals
             </p>
-            <button
-              type="button"
-              className="rounded-md mt-2 bg-white px-20 py-2 text-sm font-semibold text-black shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 border border-solid border-black"
-            >
-              <div className="flex  ">
-                <img src={GoogleImage} alt="Not found" />
-                <p className=" ml-3">Connect with Google</p>
-              </div>
+            <button className="flex items-center justify-center px-4 py-2 border border-gray-300 rounded-md bg-white text-gray-700 hover:bg-gray-100 focus:outline-none focus:border-blue-300 focus:ring focus:ring-blue-200 w-72">
+              <img
+                src={GoogleImage}
+                alt="Google Logo"
+                className="h-5 w-5 mr-2"
+              />
+              Connect with Google
             </button>
           </div>
         </div>
@@ -289,9 +440,10 @@ function BusinessInformation() {
         <div className="flex-1 border-t border-gray-300 mt-7"></div>
         <div className="flex justify-between  flex-wrap mt-14">
           <div>
-            <button className="rounded-md text-white bg-blue-800 border-blue-800 px-20 py-2 text-sm font-semibol shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 border border-solid">
-              iam not a robot
-            </button>
+            <ReCAPTCHA
+              sitekey="6LfAUjgpAAAAABQcBX1BtSezxeoNoBDoZk9XPS7T"
+              onChange={handleChange}
+            />
           </div>
           <div>
             <button className="rounded-md mt-2 text-white bg-blue-800 border-blue-800 px-20 py-2 text-sm font-semibol shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 border border-solid ">
@@ -304,4 +456,4 @@ function BusinessInformation() {
   );
 }
 
-export default BusinessInformation;
+export default BondBailsman;
