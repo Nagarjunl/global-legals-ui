e027e5e78dcdb9d94c3cc51fc3c0bf491ad9d0f2
barathan <barathanit7@gmail.com>
2023-12-21 13:31:06 +0530

    homepage backdrop and footer design

--- src/components/BlockText.jsx
+++ src/components/BlockText.jsx
@@ -3,8 +3,8 @@ const BlockText = ({ heading, content }) => {
   return (
     <>
       <div className="grid items-end mt-10">
-        <div className="flex gap-2 max-md:text-start max-md:pl-2 ">
-          <FaRegCheckCircle className="h-[28px] w-[28px] text-gray-600 max-md:hidden" />
+        <div className="flex gap-2 max-md:text-start">
+          <FaRegCheckCircle className="h-[28px] w-[28px] text-gray-600" />
           <p className="  text-xl font-bold  grow shrink basis-auto max-md:max-w-full">
             {heading}
           </p>
