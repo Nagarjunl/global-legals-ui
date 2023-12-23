3283b622816c9756065065177c6ddd2dc39e6ba5
barathan <barathanit7@gmail.com>
2023-12-23 19:57:00 +0530

    hookform package and login validate

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -8,7 +8,6 @@ const Input = ({ id, name, type, placeholder, autoComplete }) => {
         type={type}
         placeholder={placeholder}
         autoComplete={autoComplete}
-        required
         className="block w-full rounded-md border-0 p-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  focus:ring-blue-600 sm:text-sm sm:leading-6"
       />
     </div>
