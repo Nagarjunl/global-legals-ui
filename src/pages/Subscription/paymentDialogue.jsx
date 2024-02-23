1ba7cc3559164041eaf29f021ac8d5cbda4ea8b2
barathan <barathanit7@gmail.com>
2024-02-23 23:11:39 +0530

    change name to optional

--- src/pages/Subscription/paymentDialogue.jsx
+++ src/pages/Subscription/paymentDialogue.jsx
@@ -13,6 +13,8 @@ import {
 } from "../../services/stripeAPI";
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js"
 
+import GlobalLegals from "../../assets/GlobalLegals.svg";
+
 export default function PaymentDialogue(props) {
 
     const dispatch = useDispatch();
@@ -180,22 +182,21 @@ export default function PaymentDialogue(props) {
                                                     {!isLoading &&
                                                         <div className="text-center mt-5 p-3">
                                                             <div className="flex min-h-full items-end justify-center mb-5">
-                                                                {data.images.map((img, index) => (
+                                                                {/* {data?.images.map((img, index) => (
                                                                     <img key={index} src={img} alt="Global Legals" className="text-center" />
-                                                                ))}
+                                                                ))} */}
+                                                                <img src={GlobalLegals} alt="Global Legals" className="text-center" />
                                                             </div>
 
                                                             <h3 className="lg:col-span-3 font-bold leading-[34.32px] text-[24px] mb-3">
-                                                                {data.name}
+                                                                {data?.name}
                                                             </h3>
 
                                                             <p className="mb-10">  You can cancel your plan anytime on your settings. </p>
 
                                                             <div className="pt-4">
                                                                 <CardElement />
-                                                            </div>
-
-                                                        </div>
+                                                            </div>                  </div>
                                                     }
                                                 </div >
                                             </div>
