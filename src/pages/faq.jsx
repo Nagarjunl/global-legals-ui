de63bbef523c52148d5a9a2c4d0c1c690d804156
barathan <barathanit7@gmail.com>
2024-03-12 23:05:54 +0530

    Added dialogue box and content for professional in home page

--- src/pages/faq.jsx
+++ src/pages/faq.jsx
@@ -66,14 +66,14 @@ const Faq = () => {
                                     <input
                                         type="text"
                                         className="block w-full rounded-md border-0 py-[7px] pl-10 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                        placeholder="Enter Name or State only"
+                                        placeholder="Search Question"
                                     />
                                 </div>
 
                                 <div className="max-sm:pt-3">
                                     <button type="submit"
                                         className="max-sm:w-[100%] rounded-md text-white bg-blue-800 border-blue-800 px-3 py-2 text-sm font-semibol shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 border border-solid ">
-                                        Ask Qusetions
+                                        Ask Questions
                                     </button>
                                 </div>
                             </div>
